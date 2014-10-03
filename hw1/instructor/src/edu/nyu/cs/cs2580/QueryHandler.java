35500906c5039779599fb82708860102cc45f376
joshua924 <zhoushuang924@hotmail.com>
2014-10-03 03:18:14 -0400

    generating files now

--- hw1/instructor/src/edu/nyu/cs/cs2580/QueryHandler.java
+++ hw1/instructor/src/edu/nyu/cs/cs2580/QueryHandler.java
@@ -1,12 +1,17 @@
 package edu.nyu.cs.cs2580;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.Iterator;
 import java.util.Vector;
@@ -18,14 +23,72 @@ import java.net.URLDecoder;
  *
  */
 class QueryHandler implements HttpHandler {
-  /*private static String plainResponse =
-      "Request received, but I am not smart enough to echo yet!\n";*/
+
   private String resultFormat = "not_specified";
   private Ranker _ranker;
   
   //Constructor
-  public QueryHandler(Ranker ranker){
+  public QueryHandler(Ranker ranker) throws IOException{
     _ranker = ranker;
+    
+    System.out.println("Generating results ...");
+    //create corresponding files
+    File file1 = new File("data/hw1.1-vsm.tsv");
+    if(!file1.exists()){
+    	file1.createNewFile();
+    }
+    File file2 = new File("data/hw1.1-ql.tsv");
+    if(!file2.exists()){
+    	file2.createNewFile();
+    }
+    File file3 = new File("data/hw1.1-phrase.tsv");
+    if(!file3.exists()){
+    	file3.createNewFile();
+    }
+    File file4 = new File("data/hw1.1-numviews.tsv");
+    if(!file4.exists()){
+    	file4.createNewFile();
+    }
+    PrintWriter out1 = new PrintWriter(file1);
+    PrintWriter out2 = new PrintWriter(file2);
+    PrintWriter out3 = new PrintWriter(file3);
+    PrintWriter out4 = new PrintWriter(file4);
+    //read in queries from queries.tsv, run them and write to files
+    Scanner sc = new Scanner(new File("data/queries.tsv"));
+    sc.useDelimiter("\n");
+    int count_of_queries = 0;
+    while(sc.hasNext()){
+    	count_of_queries++;
+    	String que = sc.next();
+    	que = que.substring(0, que.length()-1);
+    	Vector < ScoredDocument > cos = _ranker.runquery(que,"cosine");
+    	out1.print(ScoredDocumentToString(que, cos));
+    	Vector < ScoredDocument > QL = _ranker.runquery(que,"QL");
+    	out2.print(ScoredDocumentToString(que, QL));
+    	Vector < ScoredDocument > phr = _ranker.runquery(que,"phrase");
+    	out3.print(ScoredDocumentToString(que, phr));
+    	Vector < ScoredDocument > lin = _ranker.runquery(que,"linear");
+    	out4.print(ScoredDocumentToString(que, lin));
+    }
+	out1.close(); out2.close(); out3.close(); out4.close(); sc.close();
+    System.out.println("Done generating results for " + count_of_queries + " queries...");
+  }
+  
+  public static String ScoredDocumentToString(String query, Vector < ScoredDocument > sds){
+	  String queryResponse = "";
+	  Iterator < ScoredDocument > itr = sds.iterator();
+      while (itr.hasNext()){
+        ScoredDocument sd = itr.next();
+        if (queryResponse.length() > 0){
+          queryResponse = queryResponse + "\n";
+        }
+        // Transfer a ScoredDocument into a String
+        queryResponse = queryResponse + query + "\t" + sd.asString();
+      }
+      if (queryResponse.length() > 0){
+        queryResponse = queryResponse + "\n";
+      }
+      return queryResponse;
   }
 
   // Store attributes(keys) and values in a hash map
@@ -86,53 +149,31 @@ class QueryHandler implements HttpHandler {
               queryResponse = (ranker_type+" not implemented yet.");
             }
             // process the result from ranker and generate String
-            Iterator < ScoredDocument > itr = sds.iterator();
-            while (itr.hasNext()){
-              ScoredDocument sd = itr.next();
-              if (queryResponse.length() > 0){
-                queryResponse = queryResponse + "\n";
-              }
-              // Transfer a ScoredDocument into a String
-              queryResponse = queryResponse + query_map.get("query") + "\t" + sd.asString();
-            }
-            if (queryResponse.length() > 0){
-              queryResponse = queryResponse + "\n";
-            }
+            queryResponse = ScoredDocumentToString(query_map.get("query"), sds);
           } else {
             // if no ranker type is specified, use Linear Model
             Vector < ScoredDocument > sds = _ranker.runquery(query_map.get("query"),"default");
-            Iterator < ScoredDocument > itr = sds.iterator();
-            while (itr.hasNext()){
-              ScoredDocument sd = itr.next();
-              if (queryResponse.length() > 0){
-                queryResponse = queryResponse + "\n";
-              }
-              // Transfer a ScoredDocument into a String
-              queryResponse = queryResponse + query_map.get("query") + "\t" + sd.asString();
-            }
-            if (queryResponse.length() > 0){
-              queryResponse = queryResponse + "\n";
-            }
+            queryResponse = ScoredDocumentToString(query_map.get("query"), sds);
           }
         }
       }
     }
     
-      // Construct a simple response.
-      Headers responseHeaders = exchange.getResponseHeaders();
-      OutputStream responseBody = exchange.getResponseBody();
-      if(resultFormat.equals("text")||resultFormat.equals("not_specified")){
-        responseHeaders.set("Content-Type", "text/plain");
-        exchange.sendResponseHeaders(200, 0);  // arbitrary number of bytes
-        responseBody.write(queryResponse.getBytes());
-      } else if(resultFormat.equals("html")){
-    	responseHeaders.set("Content-Type", "text/html");
-    	exchange.sendResponseHeaders(200, 0);  // arbitrary number of bytes
-    	String htmlContent = "<head><link rel=\"stylesheet\" href=\"css/style.css\"></head>"
-    	+"<p>Search result:<br></p>" + queryResponse.replace("\n", "<br>")
-    	+ "<p id=\"backToSearch\">Back to Home</p>";
-    	responseBody.write(htmlContent.getBytes());
-      }
-      responseBody.close();
+    // Construct a simple response.
+    Headers responseHeaders = exchange.getResponseHeaders();
+    OutputStream responseBody = exchange.getResponseBody();
+    if(resultFormat.equals("text")||resultFormat.equals("not_specified")){
+      responseHeaders.set("Content-Type", "text/plain");
+      exchange.sendResponseHeaders(200, 0);  // arbitrary number of bytes
+      responseBody.write(queryResponse.getBytes());
+    } else if(resultFormat.equals("html")){
+      responseHeaders.set("Content-Type", "text/html");
+      exchange.sendResponseHeaders(200, 0);  // arbitrary number of bytes
+      String htmlContent = "<head><link rel=\"stylesheet\" href=\"css/style.css\"></head>"
+      +"<p>Search result:<br></p>" + queryResponse.replace("\n", "<br>")
+      + "<p id=\"backToSearch\">Back to Home</p>";
+      responseBody.write(htmlContent.getBytes());
+    }
+    responseBody.close();
   }
 }
