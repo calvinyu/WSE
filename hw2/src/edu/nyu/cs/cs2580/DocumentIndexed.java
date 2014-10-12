253048c7fc5fe99eb55b268a161c6528ac261911
Kentaro Hanaki <kh1715@nyu.edu>
2014-10-12 17:13:06 -0400

    Interface defined.

--- hw2/src/edu/nyu/cs/cs2580/DocumentIndexed.java
+++ hw2/src/edu/nyu/cs/cs2580/DocumentIndexed.java
@@ -1,13 +1,41 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Vector;
+
 /**
  * @CS2580: implement this class for HW2 to incorporate any additional
  * information needed for your favorite ranker.
  */
 public class DocumentIndexed extends Document {
   private static final long serialVersionUID = 9184892508124423115L;
+  private Indexer _indexer = null;
+  private Vector<String> _qv = new Vector<String>(); // Not sure about the API.
 
-  public DocumentIndexed(int docid) {
+  private Vector<Integer> _titleTokens = new Vector<Integer>();
+  private Vector<Integer> _bodyFreqs = new Vector<Integer>();
+
+  public DocumentIndexed(int docid, Query q, Indexer indexer) {
     super(docid);
+    _qv = q._tokens;
+    _indexer = indexer;
   }
+
+  // Get term frequencies in the corpus. These can be used for regularization.
+  public long getTotalTermFrequency() { return _indexer.totalTermFrequency(); }
+  public long getTermFrequency(String s) { return _indexer.corpusTermFrequency(s); }
+
+  // Set and get title tokens.
+  public void setTitleTokens(Vector<Integer> titleTokens) { _titleTokens = titleTokens; }
+  public Vector<Integer> getTitleTokens() { return _titleTokens; }
+
+  // Set and get term frequencies for a document. This is used to compute the language model probs.
+  public void setBodyTokens(Vector<Integer> bodyTokens) {
+    for (String q : _qv) {
+      _bodyFreqs.add(_indexer.documentTermFrequency(q, getUrl()));
+    }
+  }
+  public Vector<Integer> getBodyTokens() { return _bodyFreqs; }
+
+  // Get length of the documents. This should be stored when indexing.
+  public int getDocLength(int did) { return 1; }
 }
