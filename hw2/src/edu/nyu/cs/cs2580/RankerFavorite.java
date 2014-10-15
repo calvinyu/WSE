8c56be004e9ffb8aa88a3d0b1835c3a6a364162f
Calvin Yu <ycy247@nyu.edu>
2014-10-15 01:03:02 -0400

    RankerFavorite w/ language model implemented

--- hw2/src/edu/nyu/cs/cs2580/RankerFavorite.java
+++ hw2/src/edu/nyu/cs/cs2580/RankerFavorite.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 
 import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
 import edu.nyu.cs.cs2580.SearchEngine.Options;
+import java.util.*;
 
 /**
  * @CS2580: Implement this class for HW2 based on a refactoring of your favorite
@@ -13,14 +14,63 @@ import edu.nyu.cs.cs2580.SearchEngine.Options;
  */
 public class RankerFavorite extends Ranker {
 
+  private Double[] beta = {2., 0.1, 0.005, 0.000005};
+
   public RankerFavorite(Options options,
       CgiArguments arguments, Indexer indexer) {
     super(options, arguments, indexer);
+
     System.out.println("Using Ranker: " + this.getClass().getSimpleName());
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public Vector<ScoredDocument> runQuery(Query query, int numResults) {
-    return null;
+    Vector < ScoredDocument > retrieval_results = new Vector < ScoredDocument > ();
+    //retrieve relavant docs
+    Document nextDoc = _indexer.nextDoc(query, -1);// not sure abou api
+    while(nextDoc != null){
+      retrieval_results.add(runquery(query, nextDoc));
+      nextDoc = _indexer.nextDoc(query, nextDoc._docid);
+    }
+    Collections.sort(retrieval_results);
+    return retrieval_results;  
+  }
+
+  public ScoredDocument runquery(Query query, Document doc){
+    Vector < Double > lmprob;
+    double score = 0.;
+    lmprob = createLmprob(doc, query, 0.5);
+    score = language_model_score(lmprob);
+    return new ScoredDocument(doc, score);
+  }
+
+  private Vector <Double> createLmprob(Document d, Query query, double lamb) {
+    DocumentIndexed doc = (DocumentIndexed) d;
+    Vector < Double > lmprob = new Vector < Double >();
+    //shouldn't this be static? or why does it need docid?
+    int length = doc.getDocLength(doc._docid);
+    // Build query vector, it should support phrase query.
+    Vector < Integer > bv = doc.getBodyTokens();
+    Iterator< Integer > bvit = bv.iterator();
+    Iterator< String > qit = query._tokens.iterator();
+    while ( bvit.hasNext() ) {
+      double score = bvit.next();
+      String s = qit.next();
+      // Add query words to language model probability map.
+      score /= length;
+      // Smoothing.
+      long tf = doc.getTermFrequency(s);
+      long totalTf = doc.getTotalTermFrequency();
+      score = lamb * score + (1 - lamb) * ( tf / totalTf );
+      lmprob.add(score);
+    }
+    return lmprob;
+  }
+
+  private double language_model_score(Vector < Double > lmprob) {
+    double lm_score = 0.;
+    for (Double score : lmprob) { lm_score += Math.log(score); }
+    return lm_score;
   }
 }
