b47485a10b20711fafaf1f48abf1ff4f34c5881b
Calvin Yu <ycy247@nyu.edu>
2014-10-18 13:37:07 -0400

    modified DocumentIndexed from K

--- hw2/src/edu/nyu/cs/cs2580/DocumentIndexed.java
+++ hw2/src/edu/nyu/cs/cs2580/DocumentIndexed.java
@@ -12,6 +12,7 @@ public class DocumentIndexed extends Document {
   private Indexer _indexer;
   private HashMap< Integer, Vector<Integer>> _body = new HashMap< Integer, Vector<Integer>>();
   private Vector<Integer> _freqList = new Vector<Integer>();
+  private int _length = 0;
 
   // constructor.
   public DocumentIndexed(int docid, Indexer indexer) {
@@ -23,13 +24,13 @@ public class DocumentIndexed extends Document {
   public void setBody(Vector<Integer> doc) {
     for (int i = 0; i < doc.size(); i++) {
       int ind = doc.get(i);
-      if (!_body.containsKey(ind)) {_body.put(ind, new Vector<Integer>()); }
+      if (_body.containsKey(ind)) {_body.put(ind, new Vector<Integer>()); }
       _body.get(ind).add(i);
     }
   }
   // set document length (to compute language model probabilities).
   public void setLength(Vector<Integer> doc) {
-    length = doc.size();
+    _length = doc.size();
   }
   // set termFrequency.
   public void setTermFrequencyList(Vector<Integer> freqList) { _freqList = freqList; }
@@ -40,5 +41,9 @@ public class DocumentIndexed extends Document {
   // get term positions (for phrases).
   public Vector<Integer> getTermPositions(int i) {
     return _body.get(i);
+  }
+  // get document length.
+  public int getLength() {
+    return _length;
   }
 }
