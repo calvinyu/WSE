676b6b41e563f5d02abf50ed5eadd44c67a45a33
Calvin Yu <ycy247@nyu.edu>
2014-10-20 22:09:59 -0400

    Query->QueryPhrase

--- hw2/src/edu/nyu/cs/cs2580/QueryHandler.java
+++ hw2/src/edu/nyu/cs/cs2580/QueryHandler.java
@@ -154,7 +154,7 @@ class QueryHandler implements HttpHandler {
     }
 
     // Processing the query.
-    Query processedQuery = new Query(cgiArgs._query);
+    QueryPhrase processedQuery = new QueryPhrase(cgiArgs._query);
     processedQuery.processQuery();
 
     // Ranking.
