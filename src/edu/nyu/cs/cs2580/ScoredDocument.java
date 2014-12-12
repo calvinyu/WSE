f0750824683d4411d68bc8780dfc398691178b72
joshua924 <zhoushuang924@hotmail.com>
2014-12-12 14:10:27 -0500

    add snippet to asHTMLResult

--- src/edu/nyu/cs/cs2580/ScoredDocument.java
+++ src/edu/nyu/cs/cs2580/ScoredDocument.java
@@ -1,6 +1,11 @@
 package edu.nyu.cs.cs2580;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * Document with score.
@@ -9,24 +14,24 @@ import java.util.*;
  * @author congyu
  */
 class ScoredDocument implements Comparable<ScoredDocument> {
-  private Document _doc;
+  private edu.nyu.cs.cs2580.Document _doc;
   private double _score;
   private float _pagerank;
   private int _numviews;
 
-  public ScoredDocument(Document doc, double score) {
+  public ScoredDocument(edu.nyu.cs.cs2580.Document doc, double score) {
     _doc = doc;
     _score = score;
   }
 
-  public ScoredDocument(Document doc, double score, float pagerank, int numviews) {
+  public ScoredDocument(edu.nyu.cs.cs2580.Document doc, double score, float pagerank, int numviews) {
     _doc = doc;
     _score = score;
     _pagerank = pagerank;
     _numviews = numviews;
   }
 
-  public Document getDoc() { return _doc; }
+  public edu.nyu.cs.cs2580.Document getDoc() { return _doc; }
   public double getScore() { return _score; }
   public void setScore(double score) { _score = score; }
   public void setPagerank(float pagerank) { _pagerank = pagerank; }
@@ -45,9 +50,6 @@ class ScoredDocument implements Comparable<ScoredDocument> {
   /**
    * @CS2580: Student should implement {@code asHtmlResult} for final project.
    */
-  public String asHtmlResult() {
-    return "";
-  }
 
   @Override
   public int compareTo(ScoredDocument o) {
@@ -57,16 +59,34 @@ class ScoredDocument implements Comparable<ScoredDocument> {
     return (this._score > o._score) ? 1 : -1;
   }
 
-  public String asHtmlResult(String query) {
-  StringBuffer buf = new StringBuffer();
-  Date dd = new Date();
-  String sessionid = "S-"+String.valueOf(dd.getTime()).substring(5);
-  String hyperlink1 = "sessionid=" + sessionid + "&action=click&query="
-    + query + "&docid=" + _doc._docid;
-  buf.append("<p style=\"font-size:14px; margin-top:14px\">"
-      + "<a href=\"http://localhost:25813/log?" + hyperlink1
-          + "\" target=\"_blank\" style=\"text-decoration: none\">");
-  buf.append(_doc.getTitle()).append("</a></p>");
-  return buf.toString();
+  public String asHtmlResult(String query) throws IOException {
+	// append title and hyperlink information
+    StringBuffer buf = new StringBuffer();
+    String hyperlink1 = "http://en.wikipedia.org/wiki/" + _doc.getTitle();
+    buf.append("<p><a href=" + hyperlink1
+          + " target=\"_blank\" style=\"text-decoration: none\">");
+    buf.append(_doc.getTitle()).append("</a></p><br>");
+    // append snippet
+    buf.append("<span style=\"font-size:14px; width:60%\">");
+    String prefix = SearchEngine.OPTIONS._corpusPrefix;
+    File correspondingFile = new File(prefix + "\\" + _doc.getTitle());
+    Document DOM = Jsoup.parse(correspondingFile, "UTF-8", "");
+    String content = DOM.select("#bodyContent").text().toLowerCase();
+    int index = 0;
+	if(content.contains(" " + query + " ")){
+		index = content.indexOf(query);
+		int start = content.substring(0, index).lastIndexOf(". ");
+		if(start != -1){
+			buf.append(content.substring(start + 1, index) + "<b><em>" + query + "</em></b>"
+					+ content.substring(index + query.length(), 
+							index + query.length() + 30) + "...");
+		} else {
+			buf.append("<em><b>" + query + "</b></em>" + content.substring(index + query.length(),
+					index + query.length() + 30)  + " ... ");
+		}
+	}
+	buf.append("</span>");
+    return buf.toString();
   }
+
 }
