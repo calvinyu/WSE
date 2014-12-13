30c115c5ac5bcf5e75460be31628684bfdfa4e22
joshua924 <zhoushuang924@hotmail.com>
2014-12-13 00:06:49 -0500

    If whole phrase is not found, try smaller phrases
    just try one step, works fine, if needed, can extend further

--- src/edu/nyu/cs/cs2580/ScoredDocument.java
+++ src/edu/nyu/cs/cs2580/ScoredDocument.java
@@ -62,31 +62,51 @@ class ScoredDocument implements Comparable<ScoredDocument> {
   public String asHtmlResult(String query) throws IOException {
 	// append title and hyperlink information
     StringBuffer buf = new StringBuffer();
-    String hyperlink1 = "http://en.wikipedia.org/wiki/" + _doc.getTitle();
+    String hyperlink1 = "file:///D:/In%20USA/Courses/Web%20Search%20Engines/" + 
+    		"Workspace/SearchEngine" + "/data/wiki/" + _doc.getTitle();
     buf.append("<p><a href=" + hyperlink1
           + " target=\"_blank\" style=\"text-decoration: none\">");
-    buf.append(_doc.getTitle()).append("</a></p><br>");
+    buf.append(_doc.getTitle()).append("</a></p>");
+    
     // append snippet
+    // snippet style
     buf.append("<span style=\"font-size:14px; width:60%\">");
+    // read file
     String prefix = SearchEngine.OPTIONS._corpusPrefix;
     File correspondingFile = new File(prefix + "/" + _doc.getTitle());
+    // process file
     Document DOM = Jsoup.parse(correspondingFile, "UTF-8", "");
     String content = DOM.select("#bodyContent").text().toLowerCase();
-    int index = 0;
+    // find snippet
 	if(content.contains(" " + query + " ")){
-		index = content.indexOf(query);
-		int start = content.substring(0, index).lastIndexOf(". ");
-		if(start != -1){
-			buf.append(content.substring(start + 1, index) + "<b><em>" + query + "</em></b>"
-					+ content.substring(index + query.length(), 
-							index + query.length() + 30) + "...");
+		addToBuf(buf, content, query);
+	} else if (query.contains(" ")){
+		// else should also try to present something if it's a phrase
+		String firstPart = query.substring(0, query.lastIndexOf(" "));
+		if(content.contains(firstPart)){
+			addToBuf(buf, content, firstPart);
 		} else {
-			buf.append("<em><b>" + query + "</b></em>" + content.substring(index + query.length(),
-					index + query.length() + 30)  + " ... ");
+			String secondPart = query.substring(query.indexOf(" ") + 1);
+			if (content.contains(secondPart))
+				addToBuf(buf, content, secondPart);
 		}
 	}
+	// wrap and return it
 	buf.append("</span>");
     return buf.toString();
+  }
+
+  public static void addToBuf(StringBuffer buf, String content, String query) {
+	  int index = content.indexOf(query);
+	  int start = content.substring(0, index).lastIndexOf(". ");
+	  if(start != -1){
+		buf.append(content.substring(start + 1, index) + "<b><em>" + query + "</em></b>"
+				+ content.substring(index + query.length(), 
+						index + query.length() + 40) + "...");
+	  } else {
+		buf.append("<em><b>" + query + "</b></em>" + content.substring(index + 
+				query.length(), index + query.length() + 40)  + " ... ");
+	  }
   }
 
 }
